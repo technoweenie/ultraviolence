c6a70fa3250ac932ee95c1fa08205611a62b077f
rick <technoweenie@gmail.com>
2010-02-05 14:31:54 -0800

    dynamic css loading

--- public/javascripts/uv.js
+++ public/javascripts/uv.js
@@ -10,7 +10,23 @@ google.setOnLoadCallback(function() {
       $('#tabs-result').html(result);
       $('#src').text(result);
       nav.tabs('select', 1);
+      loadTheme($('#theme').val())
     })
     return false;
   });
 });
+
+var loadedThemes = {}
+
+function loadTheme(theme){
+  if(!loadedThemes[theme]) {
+    var url = "/stylesheets/themes/" + theme + ".css";
+    var fileref=document.createElement("link")
+    fileref.setAttribute("rel", "stylesheet")
+    fileref.setAttribute("type", "text/css")
+    fileref.setAttribute("href", url)
+    loadedThemes[theme] = true;
+    if (typeof fileref!="undefined")
+      document.getElementsByTagName("head")[0].appendChild(fileref)
+  }
+}
