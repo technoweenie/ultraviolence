747c7ac5b3757388156675abef8f7ae3709f4a2d
rick <technoweenie@gmail.com>
2010-02-05 11:17:01 -0800

    basic instructions

--- ultraviolence.rb
+++ ultraviolence.rb
@@ -6,6 +6,18 @@ $LOAD_PATH.unshift File.expand_path(File.join(vendor, 'ultraviolet-0.10.5',  'li
 require 'sinatra'
 require 'uv'
 
+get '/' do
+  s = <<-END
+<p>POST data to /api</p>
+<p>curl \"http://ultraviolence.heroku.com/api?s=css&l=1\" -d '.abc {}'</p>
+<ul>
+  <li><code>?s or ?syntax</code> - specify syntax</li>
+  <li><code>?t or ?theme</code> - specify textmate theme (defaults to mac classic)</li>
+  <li><code>?l or ?line_numbers</code> - set to '1' to show line numbers</li>
+</ul>
+END
+end
+
 post '/api' do
   uv env['rack.input'].read, params
 end
