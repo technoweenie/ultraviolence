050b18eb16c9c77558d964fed92d88b3032602bb
rick <technoweenie@gmail.com>
2010-03-19 10:46:10 -0700

    show a link to the theme css file

--- public/javascripts/uv.js
+++ public/javascripts/uv.js
@@ -2,21 +2,22 @@ google.load("jquery",   "1.4.1");
 google.load("jqueryui", "1.7.2");
 
 google.setOnLoadCallback(function() {
-  var nav = $('#tabs')
+  var nav = $('#tabs');
   nav.tabs();
 
   nav.bind('tabsselect', function(event, ui) {
-    window.location.hash = ui.tab.hash
+    window.location.hash = ui.tab.hash;
   });
 
   $('#uv-form').submit(function() {
-    var data = $(this).serialize();
+    var theme = $('#theme').val(),
+         data = $(this).serialize();
     $.get("/api", data, function(result) {
-      $('#tabs-result').html(result);
+      $('#tabs-result').html(themeLink(theme) + result);
       $('#src').text(result);
       nav.tabs('select', 1);
       loadTheme($('#theme').val())
-    })
+    });
     return false;
   });
 });
@@ -26,12 +27,17 @@ var loadedThemes = {}
 function loadTheme(theme){
   if(!loadedThemes[theme]) {
     var url = "/stylesheets/themes/" + theme + ".css";
-    var fileref=document.createElement("link")
-    fileref.setAttribute("rel", "stylesheet")
-    fileref.setAttribute("type", "text/css")
-    fileref.setAttribute("href", url)
+    var fileref=document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", url);
     loadedThemes[theme] = true;
     if (typeof fileref!="undefined")
       document.getElementsByTagName("head")[0].appendChild(fileref)
   }
+}
+
+function themeLink(theme) {
+  var link = "/stylesheets/themes/" + theme + ".css"
+  return "<p>Download the <a href=\"" + link + "\" title=\"Please don&apos;t hotlink this :)\">" + theme + "</a> theme.</p>"
 }
